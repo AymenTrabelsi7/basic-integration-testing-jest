dd78e3a9bb7ec408bd6782beef4a6cd95a67b478
Aymen <tr.aymen@protonmail.com>
2022-01-28 18:01:48 +0100

    Added first tests for GET /todos

--- test/todos.test.js
+++ test/todos.test.js
@@ -16,19 +16,37 @@ afterAll(async () => {
     closeConnection()
 })
 
+afterEach(async() => {
+    const db = getDB()
+    await db.dropCollection("todos")
+})
+
+beforeEach(async() => {
+    const db = getDB()
+    await db.createCollection("todos")
+})
+
 describe("GET /todos", () => {
     test("should respond with a 200 status code", async () => {
-        // TODO
-        fail("Not yet implemented")
+        const response = await request(app.callback()).get(baseUrl)
+        expect(response.statusCode).toBe(200)
     })
 
     test("should respond with JSON", async () => {
-        // TODO
-        fail("Not yet implemented")
+        const response = await request(app.callback()).get(baseUrl)
+        expect(response.type).toBe("application/json")
     })
 
     test("should respond with list of existing todos", async () => {
-        // TODO
-        fail("Not yet implemented")
+        const todoObj = {title: "TodoTest", completed: false, completedAt: "2022-01-20T09:54:48.139Z", updatedAt: "2022-01-20T09:54:48.139Z"}
+        const db = getDB()
+        await db.collection("todos").insertOne(todoObj)
+        const response = await request(app.callback()).get(baseUrl)
+        console.log(response.body)
+        todos = response.body
+        expect(todos.length).toBe(1)
+        todoToTest = todos[0]
+        expect(todoToTest.title).toBe("TodoTest")
+        expect(todoToTest.completed).toBe(false)
     })
 })
